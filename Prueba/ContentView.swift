7f674bede755d4908849aee2e8d88361dda1ca57
Daniela Lara <laradni@icloud.com>
2019-09-25 17:09:00 -0500

    Initial Commit

---
+++ Prueba/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Prueba
+//
+//  Created by DANIELA LARA LOZANO on 9/25/19.
+//  Copyright © 2019 DANIELA LARA LOZANO. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
